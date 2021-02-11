ba359f44c441a6840747392e4c88410279e0fcfe
Niels Vegter <niels@denes.nl>
2021-02-11 10:07:12 +0000

    Bugfix: previous/next buttons - related ticket https://trello.com/c/EPbOWSwZ

--- src/previous-next-button-block/component/previous-next-button-block.jsx
+++ src/previous-next-button-block/component/previous-next-button-block.jsx
@@ -15,17 +15,25 @@ export default class OpenStadComponentPreviousNextButtonBlock extends OpenStadCo
 		let self = this;
 
     let previousButtonHTML;
-    let previousAction = this.props.previousAction;
-    if (this.props.previousUrl) previousAction = () => { document.location.href = `${this.props.previousUrl}` };
+    let previousAction = this.props.previousAction || this.config.previousAction;
+    let previousUrl = this.props.previousUrl || this.config.previousUrl;
+    let previousIsDisabled = this.props.previousIsDisabled || this.config.previousIsDisabled;
+    let previousLabel = this.props.previousLabel || this.config.previousLabel;
+    
+    if (previousUrl) previousAction = () => { document.location.href = `${previousUrl}` };
     if ( previousAction ) {
-      previousButtonHTML = <div className={`osc-previous-button${this.props.previousIsDisabled ? ' osc-disabled' : ''}`} onClick={(args) => previousAction(args) }>{this.props.previousLabel}</div>
+      previousButtonHTML = <div className={`osc-previous-button${previousIsDisabled ? ' osc-disabled' : ''}`} onClick={(args) => previousAction(args) }>{previousLabel}</div>
     }
 
     let nextButtonHTML;
-    let nextAction = this.props.nextAction;
-    if (this.props.nextUrl) nextAction = () => { document.location.href = `${this.props.nextUrl}` };
+    let nextAction = this.props.nextAction || this.config.nextAction;
+    let nextUrl = this.props.nextUrl || this.config.nextUrl;
+    let nextIsDisabled = this.props.nextIsDisabled || this.config.nextIsDisabled;
+    let nextLabel = this.props.nextLabel || this.config.nextLabel;
+
+    if (nextUrl) nextAction = () => { document.location.href = `${nextUrl}` };
     if ( nextAction ) {
-      nextButtonHTML = <div className={`osc-next-button${this.props.nextIsDisabled ? ' osc-disabled' : ''}`} onClick={(args) => nextAction(args) }>{this.props.nextLabel}</div>
+      nextButtonHTML = <div className={`osc-next-button${nextIsDisabled ? ' osc-disabled' : ''}`} onClick={(args) => nextAction(args) }>{nextLabel}</div>
     }
 
     return (
