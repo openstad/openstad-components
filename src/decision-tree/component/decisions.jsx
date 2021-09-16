3f6a3ab8d5b2d54fb178bba91c84357976d7c8ae
stan turner <stanturner@MacBook-Pro-van-stan.local>
2021-09-16 10:13:34 +0200

    changed styling of decision tree

--- src/decision-tree/component/decisions.jsx
+++ src/decision-tree/component/decisions.jsx
@@ -1,26 +1,15 @@
 export function Decisions({ activeDecision }) {
-    return (
-      <ul>
-        {activeDecision.decisions.map((decision) => (
-          <li key={decision.id}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="48px"
-              viewBox="0 0 24 24"
-              width="48px"
-              fill="#000000"
-            >
-              <path d="M0 0h24v24H0V0z" fill="none" />
-              <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6-6-6z" />
-            </svg>
-  
-            <label>
-              <input type="radio" name="decision-tree" value={decision.id} />{" "}
-              {decision.title}
-            </label>
-          </li>
-        ))}
-      </ul>
-    );
+  return (
+  <ul>
+     {activeDecision.decisions.map((decision) => (
+     <li key={decision.id}>
+        <input type="radio" id={decision.id} name="decision-tree" value={decision.id} />{" "}
+        <label for={decision.id}>
+        {decision.title}
+        </label>
+     </li>
+     ))}
+  </ul>
+  );
   }
   
