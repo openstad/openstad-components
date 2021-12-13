11bc851342e9e2de20e92bdbd5009711f965bafe
Niels Vegter <niels@denes.nl>
2021-12-13 15:12:15 +0000

    Add button component, including numberplates

--- src/index.jsx
+++ src/index.jsx
@@ -21,29 +21,28 @@ import Libs from './libs/index.jsx';
 import Forms from './forms/index.jsx';
 import { ChoicesGuide, ChoicesGuideResult } from './choices-guide/index.jsx';
 import BaseMap from './base-map/index.jsx';
+import Button from './button/index.jsx';
 import Lightbox from './lightbox/index.jsx';
 import IdeasOnMap from './ideas-on-map/index.jsx';
 import IdeaDetails from './idea-details/index.jsx';
 import IdeasOverview from './ideas-overview/index.jsx';
 import IdeasImage from './image/index.jsx';
-import Numberplates from './numberplates/index.jsx';
 import Poll from './poll/index.jsx';
 import PreviousNextButtonBlock from './previous-next-button-block/index.jsx';
 import Reactions from './reactions/index.jsx';
 
 export {
   OpenStadComponent,
-  Libs,
   Forms,
   ChoicesGuide,
   ChoicesGuideResult,
   BaseMap,
+  Button,
   IdeasOnMap,
   IdeaDetails,
   IdeasOverview,
   IdeasImage,
   Lightbox,
-  Numberplates,
   Poll,
   PreviousNextButtonBlock,
   Reactions,
